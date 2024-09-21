07037558f320d1670c61f5cb2bb7dfe18906c265
BoggTech <113841718+BoggTech@users.noreply.github.com>
2024-09-21 22:26:16 +0100

    localToon.js + toontask overlays

---
+++ js/localToon.js
@@ -0,0 +1,239 @@
+const ENDPOINT = "info.json"
+const URL = "localhost"
+const VALID_TRACKS = ["Toon-Up", "Trap", "Lure", "Sound", "Throw", "Squirt", "Drop"]
+
+function isValidTrack(track) {
+    return VALID_TRACKS.includes(track);
+}
+
+class ToonData {
+    constructor(port, token, user_agent) {
+        this.port = port;
+        this.token = token;
+        this.user_agent = user_agent;
+        this.data = null;
+    }
+
+    async updateData(auth=this.token) {
+        const url = `http://${URL}:${this.port}/${ENDPOINT}`;
+        const headers = {"User-Agent": this.user_agent, 
+                        "Authorization": auth, 
+                        "Connection": 'close'}
+        try {
+            const response = await fetch(url, { headers });
+            if (!response.ok) {
+                throw {message: `HTTP error ${response.status}`, code:response.status};
+            }
+            this.data = await response.json();
+        } catch (error) {
+            // todo: something... maybe...?
+            console.log(error);
+            throw(error);
+        }
+    }
+
+    getName() {
+        if (this.data == null) return null;
+        return this.data.toon.name;
+    }
+
+    getSpecies() {
+        if (this.data == null) return null;
+        return this.data.toon.species;
+    }
+
+    getHeadColor() {
+        if (this.data == null) return null;
+        return this.data.toon.headColor;
+    }
+
+    getStyle() {
+        if (this.data == null) return null;
+        return this.data.toon.style;
+    }
+
+    getCurrentLaff() {
+        if (this.data == null) return null;
+        return this.data.laff.current;
+    }
+
+    getMaxLaff() {
+        if (this.data == null) return null;
+        return this.data.laff.max;
+    }
+
+    getZone() {
+        if (this.data == null) return null;
+        return this.data.location.zone;
+    }
+
+    getNeighborhood() {
+        if (this.data == null) return null;
+        return this.data.location.neighborhood;
+    }
+
+    getDistrict() {
+        if (this.data == null) return null;
+        return this.data.location.district;
+    }
+
+    hasTrack(track) {
+        if (this.data == null || !isValidTrack(track)) return null;
+        try {
+            return (this.data.gags[track] != null);
+        } 
+        catch (error) {
+            return false;
+        }
+    }
+
+    getHighestGagLevel(track) {
+        if (this.data == null || !isValidTrack(track)) return null; 
+        if (!this.hasTrack(track)) return 0;
+        return this.data.gags[track].gag.level;
+    }
+
+    getHighestGagName(track) {
+        if (this.data == null || !this.hasTrack(track)) return null;
+        return this.data.gags[track].gag.name;
+    }
+
+    hasAnyOrganic(track) {
+        return (this.getHighestOrganicLevel(track) != 0 && this.getHighestOrganicLevel(track) != null);
+    }
+
+    getHighestOrganicLevel(track) {
+        if (this.data == null || !isValidTrack(track)) return null;
+        if ( this.hasTrack(track) && this.data.gags[track].organic != null ) {
+            return this.data.gags[track].organic.level
+        } else {
+            return 0;
+        }
+    }
+
+    getHighestOrganicName(track) {
+        if (this.data == null || !isValidTrack(track)) return null;
+        if ( this.hasTrack(track) && this.data.gags[track].organic != null ) {
+            return this.data.gags[track].organic.name
+        } else {
+            return null;
+        }
+    }
+
+    hasAnyOrganic(track) {
+        return this.getHighestOrganicName(track) != null
+    }
+
+    getCurrentExperience(track) {
+        if (this.data == null || !this.hasTrack(track) ) return null;
+        return this.data.gags[track].experience.current
+    }
+
+    getNextExperience(track) {
+        if (this.data == null || !this.hasTrack(track) ) return null;
+        return this.data.gags[track].experience.next
+    }
+
+    hasTaskInSlot(slot) {
+        // this doesn't "technically" line up with the in-game tasks, but that isn't given to us by the api.
+        if (this.data == null) return null
+        return slot < this.data.tasks.length
+    }
+
+    getTaskObjective(slot) {
+        if (this.data == null || !this.hasTaskInSlot(slot)) return null
+        return this.data.tasks[slot].objective.text;
+    }
+
+    getTaskLocation(slot) {
+        if (this.data == null || !this.hasTaskInSlot(slot)) return null
+        return this.data.tasks[slot].objective.where;
+    }
+
+    getTaskProgressText(slot) {
+        // returns task progress as string, localized.
+        if (this.data == null || !this.hasTaskInSlot(slot)) return null
+        return this.data.tasks[slot].objective.progress.text;
+    }
+
+    getCurrentTaskProgress(slot) {
+        if (this.data == null || !this.hasTaskInSlot(slot)) return null
+        return this.data.tasks[slot].objective.progress.current;
+    }
+
+    getCurrentTaskTarget(slot) {
+        if (this.data == null || !this.hasTaskInSlot(slot)) return null
+        return this.data.tasks[slot].objective.progress.target;
+    }
+
+    getTaskFromNpcName(slot) {
+        if (this.data == null || !this.hasTaskInSlot(slot)) return null
+        return this.data.tasks[slot].from.name
+    }
+
+    getTaskFromNpcBuilding(slot) {
+        if (this.data == null || !this.hasTaskInSlot(slot)) return null
+        return this.data.tasks[slot].from.building
+    }
+
+    getTaskFromNpcZone(slot) {
+        if (this.data == null || !this.hasTaskInSlot(slot)) return null
+        return this.data.tasks[slot].from.zone
+    }
+
+    getTaskFromNpcNeighborhood(slot) {
+        if (this.data == null || !this.hasTaskInSlot(slot)) return null
+        return this.data.tasks[slot].from.neighborhood
+    }
+
+    getTaskToNpcName(slot) {
+        if (this.data == null || !this.hasTaskInSlot(slot)) return null
+        return this.data.tasks[slot].to.name
+    }
+
+    getTaskToNpcBuilding(slot) {
+        if (this.data == null || !this.hasTaskInSlot(slot)) return null
+        return this.data.tasks[slot].to.building
+    }
+
+    getTaskToNpcZone(slot) {
+        if (this.data == null || !this.hasTaskInSlot(slot)) return null
+        return this.data.tasks[slot].to.zone
+    }
+
+    getTaskToNpcNeighborhood(slot) {
+        if (this.data == null || !this.hasTaskInSlot(slot)) return null
+        return this.data.tasks[slot].to.neighborhood
+    }
+
+    getTaskReward(slot) {
+        if (this.data == null || !this.hasTaskInSlot(slot)) return null
+        return this.data.tasks[slot].reward
+    }
+
+    isTaskDeletable(slot) {
+        if (this.data == null || !this.hasTaskInSlot(slot)) return null
+        return this.data.tasks[slot].deletable
+    }
+
+    hasInvasion() {
+        if (this.data == null) return null
+        return (!(this.data.invasion == null))
+    }
+
+    getInvasionCog() {
+        if (this.data == null || !this.hasInvasion()) return null
+        return this.data.invasion.cog
+    }
+
+    getInvasionQuantity() {
+        if (this.data == null || !this.hasInvasion()) return null
+        return this.data.invasion.quantity
+    }
+
+    isInvasionMega() {
+        if (this.data == null) return null
+        if (!this.hasInvasion()) return false
+        return this.data.invasion.mega
+    }
+}
---
+++ js/toontask.js
@@ -0,0 +1,101 @@
+const timeout = 3000;
+var test;
+const token_str = "ttr-token"
+
+function update_data() {
+    test.updateData()
+    .then(update_toontasks_and_loop)
+    .catch((error) => {
+        hide_all_scrolls();
+        if (error.code == 403) {
+            // hey :(...
+            sessionStorage.removeItem(token_str);
+            setTimeout(setup_toontask_loop, timeout);
+        } else {
+            setTimeout(update_data, timeout);
+        }
+    })
+}
+
+function update_toontasks_and_loop() {
+    for ( let i = 0; i < 4; i++ ) {
+        let element = document.getElementById("task" + i)
+        if ( element == null ) {
+            // the page we're on is missing this toontask
+            continue;
+        }
+
+        if ( !test.hasTaskInSlot(i) ) {
+            // no task, hide everything
+            element.style.display = "none";
+        } else {
+            jff_indicator = document.getElementById("jff-indicator" + i);
+            jff_indicator.style.display = "none";
+            if (test.isTaskDeletable(i)) {
+                jff_indicator.style.display = "block";
+            }
+            element.style.display = "block";
+            let location = test.getTaskLocation(i);
+            let objective = test.getTaskObjective(i);
+            if (objective == "Visit") { // weird. but works. visit tasks only show as "visit"
+                objective = objective + " " + test.getTaskToNpcName(i)
+                place = test.getTaskToNpcBuilding(i);
+                if (!place)
+                    place = test.getTaskToNpcZone(i);
+                if (!place)
+                    place = test.getTaskToNpcNeighborhood(i);
+                location = place;
+            }
+            document.getElementById("text" + i).textContent = objective;
+            document.getElementById("where" + i).textContent = location;
+            progress = test.getTaskProgressText(i);
+            if ( progress ) {
+                let current = test.getCurrentTaskProgress(i);
+                let target = test.getCurrentTaskTarget(i);
+                let percent = Math.max(0, Math.min(1, current/target))
+                document.getElementById("progress" + i).style.width = (100 * percent).toString() + "%"
+                document.getElementById("bar" + i).style.display = "block";
+                document.getElementById("progress-text" + i).textContent = progress;
+            } else {
+                document.getElementById("bar" + i).style.display = "none";
+            }
+        }
+    }
+    setTimeout(update_data, timeout);
+}
+
+function hide_all_scrolls() {
+    for ( let i = 0; i < 4; i++ ) {
+        let element = document.getElementById("task" + i);
+        if ( element == null ) {
+            // the page we're on is missing this toontask
+            continue;
+        }
+        element.style.display = "none";
+    }
+}
+
+function setup_toontask_loop() {
+    hide_all_scrolls();
+    let token = sessionStorage.getItem(token_str);
+    if (token == null) {
+        // we have no token. we'll generate one.
+        token = makeid(32);
+        sessionStorage.setItem(token_str, token);
+    }
+    test = new ToonData(1547, token, "OBS Toon Overlay");
+    update_data();
+}
+
+// https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
